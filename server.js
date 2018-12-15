a03079dfbcb9f81b3de946fbd411b2327d503803
William B. Swift <willbswift@hotmail.com>
2018-12-14 18:56:35 -0600

    heroku stuff

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+// Sets an initial port. We"ll use this later in our listener
+let PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
